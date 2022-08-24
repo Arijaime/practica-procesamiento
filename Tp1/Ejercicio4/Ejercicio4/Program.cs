6d89ade93beee777add2728ba2a976b1c46925f4
Arijaime <53830508+Arijaime@users.noreply.github.com>
2022-08-24 00:42:07 -0300

    add ejercicio 4

---
+++ Tp1/Ejercicio4/Ejercicio4/Program.cs
@@ -0,0 +1,63 @@
+﻿
+
+internal class Program {
+
+    static void Main()
+    {
+        string numeroWrapper = "";
+        string digitoWrapper = "";
+        string resultadoBusqueda = "";
+        Boolean hayCoincidencia = false;
+        cargaDatos();
+
+        buscarCoincidencia();
+
+        mostrarResultado();
+
+
+
+
+
+        //método para cargar los datos 
+        void cargaDatos(){
+
+           
+
+            Console.Write("Por favor ingrese un numero :" );
+             numeroWrapper = Console.ReadLine();
+            Console.Write("Por favor ingrese un digito :");
+            digitoWrapper = Console.ReadLine();
+
+            if ((numeroWrapper !="" )&& (digitoWrapper !="")) {
+
+                Console.WriteLine("Datos Cargados Correctamente");
+
+
+            }
+           
+
+        }
+        
+        //método para buscar la coincidencia
+        void buscarCoincidencia() {
+            int longitud = numeroWrapper.Length;
+           
+            for (int i = 0; i <longitud; i++)
+            {
+                resultadoBusqueda = numeroWrapper.Substring(i,1);
+
+                if (resultadoBusqueda==digitoWrapper) {
+                    hayCoincidencia = true;                
+                }
+
+            }
+
+        }
+
+        //mostrar resultados
+        void mostrarResultado() {
+            Console.WriteLine(hayCoincidencia);
+        }
+
+    }
+}
