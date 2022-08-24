9d7aace7f470cdbe0b6c6826a06aefbcd423149f
Arijaime <53830508+Arijaime@users.noreply.github.com>
2022-08-24 00:33:38 -0300

    add console app test

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,11 @@
+﻿
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        
+        
+            Console.WriteLine( "El resultado de la suma {0} y {1} es : {2} ",1,2,1+2);
+        
+    }
+}
