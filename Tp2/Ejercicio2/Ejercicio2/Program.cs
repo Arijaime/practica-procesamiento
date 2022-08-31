f860ee8e060db21abccbeb873c9d5f42a8df4de7
tprocesamientob <tprocesamientob@server.comercio>
2022-08-31 19:55:01 -0300

    ADD ejercicio2 TP2

--- Tp2/Ejercicio2/Ejercicio2/Program.cs
+++ Tp2/Ejercicio2/Ejercicio2/Program.cs
@@ -7,15 +7,45 @@ namespace Ejercicio2
         static void Main()
         {
             string ingresoDatos;
-            int longitud;
+            int longitud,ultimoChar;
+            string nombre, apellido;
 
             Console.WriteLine("ingrese datos : ");
             ingresoDatos = Console.ReadLine();
+
+            longitud = ingresoDatos.Length;
             
 
             int posSpace = ingresoDatos.IndexOf(" ");
 
-            
+            ultimoChar = longitud - posSpace -1 ;
+
+            nombre = ingresoDatos.Substring(0, posSpace);
+            apellido = ingresoDatos.Substring((posSpace+1),ultimoChar);
+
+            nombre = toUpperMethod(nombre);
+            apellido = toUpperMethod(apellido);
+
+
+
+            Console.WriteLine("datos : {1} {0}", nombre, apellido);
+
+
+            string toUpperMethod(string str) {
+
+                
+                    string character;
+                    character = str.Substring(0,1);
+
+                    character = character.ToUpper();
+
+                str = str.Remove(0,1);
+
+                str = String.Concat(character,str);
+
+                return str;
+            }
+
 
         }
     }
